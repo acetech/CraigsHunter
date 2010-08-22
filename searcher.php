98e195edbd3af3f1b3f76f7c93593610065a8aee
Jonathan Panzica <acetech@MacPro.local>
2010-08-21 23:10:16 -0700

    fixed oop for craigslist search results

--- searcher.php
+++ searcher.php
@@ -5,6 +5,6 @@ $query = "Rolex";
 $obj = new Search();
 
 $query = "Rolex";
-$obj->search("http://rochester.craigslist.org/search/sss?query=$query&srchType=A&format=rss");
+$results = $obj->search("http://rochester.craigslist.org/search/sss?query=$query&srchType=A&format=rss");
 
-print_r($obj);
+print_r($results);
